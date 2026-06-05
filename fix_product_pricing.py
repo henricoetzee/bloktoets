caa2d2d674b46d2c6b3da37ff3bdee90435652a2
Henri Coetzee <henricoetzee@hotmail.com>
2026-06-05 08:37:32 +0200

    changed product temp price fix script4 (possible final)

--- fix_product_pricing.py
+++ fix_product_pricing.py
@@ -5,15 +5,19 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SparApps.settings')
 django.setup()
 
 from bloktoets.models import Products
+from bloktoets.views import update_recipe_pricing_all
 
 products = Products.objects.all()
 print("Store\t\t\tRecipebook\t\tProduct\t\t\t\tNet Cost\tVolume\t\tCurrent cost\tNew cost")
+
 count = 0
 for product in products:
-    count += 1
+
     correct_price = product.cost / product.volume
+  
     if not (-0.3 < correct_price - product.unit_price < 0.3):
-        volume_tabs = '\t'
+        count += 1
+        """ volume_tabs = '\t'
         if product.cost < 1000.0:
             volume_tabs += '\t'
         price_tabs = '\t'
@@ -26,6 +30,12 @@ for product in products:
         else:
             space = ' ' * (31 - len(product.name))
             print(f'{store}\t{recipebook}\t{product.name}{space}\t{product.cost:,.2f}{volume_tabs}{product.volume:,.2f}\t\t{product.unit_price:,.2f}{price_tabs}{correct_price:,.2f}')
-    #if count > 10:
-    #    break
-    
+ """
+        product.unit_price = correct_price
+        product.save(update_fields=["unit_price"])
+
+print(f"Updated {count} products. Calculating new recipe prices...")
+
+changes = update_recipe_pricing_all()
+
+print(f"Updated {len(changes)} recipe prices.")
